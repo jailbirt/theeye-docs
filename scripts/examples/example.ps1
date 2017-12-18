507e3adf27849ac0032567877e7be8a72621783f
slaplume <santiago.laplume@gmail.com>
2017-12-18 17:43:06 -0300

    Update example.ps1

--- scripts/examples/example.ps1
+++ scripts/examples/example.ps1
@@ -1,2 +1,6 @@
+<#
+Description: Prints first paramater to stdout
+Execution from cmd: powershell.exe -NonInteractive -ExecutionPolicy ByPass -File "example.ps1 Hello World"
+ #>
 Write-Output $args[0]
 Write-Output "Success"
