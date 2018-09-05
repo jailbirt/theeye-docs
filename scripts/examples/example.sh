9988092d89a289ce478deb364380f1015ab278a7
Facundo <facundo.siquot@gmail.com>
2018-09-05 12:57:44 -0300

    Update example.sh

--- scripts/examples/example.sh
+++ scripts/examples/example.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
+
 ## For both Monitors and Tasks We need that your script ends by returning "normal" or "failure". 
 STATE="normal"
-# run your command
+
+# run your command. this will generate output , but we will only parse the last line
 echo "Hello World"
+
 #change state if its need it. 
-#print state.
-if [ $? -ne 0 ];then STATE="failure"; fi
+#print state. last line of this script will be the $STATE value
+if [ $? -ne 0 ]; then STATE="failure"; fi
 echo $STATE
-## You can even retrieve more information by sending json formatted string. 
-## 
-## I.E :
-## echo '{"'$STATE'":"normal","data":{ "name1":"value1", "name2":"value2" }}'
+
+#
+# You can also generate aditional output by sending json valid string.
+# An array in the data property is required when you need to output the inputs for the next task
+# 
+# I.E :
+# echo "{\"state\":\"$STATE\",\"data\":[\"arg1\",\"arg2\",\"arg3\"]}"
+#
