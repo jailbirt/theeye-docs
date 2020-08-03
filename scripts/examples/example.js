9d8554f4036da9e1b59d8b5c6cbbb1a89b424963
Facundo <facugon@theeye.io>
2020-08-03 17:41:43 -0300

    defining nodejs boilerplate
    update boilerplate. modify for nodejs-sdk automatic error and result handler

--- scripts/examples/example.js
+++ scripts/examples/example.js
@@ -2,15 +2,9 @@
 
 // NodeJs boilerplate
 const main = async () => {
- try  {
   let data = []
-  // add code here.
-  // let response = await aHTTPPromiseCall()
-  // data.push( response )
-  successOutput(data)
- } catch (err) {
-  failureOutput(err)
- }
+  // add your code here.
+  return data
 }
 
 /**
@@ -29,20 +23,4 @@ const failureOutput = (err) => {
   console.error(JSON.stringify(output))
 }
 
-const aFunctionCall = await () => {
- // do async things here
-}
-
-/**
- * @return {Promise}
- */
-const aHTTPPromiseCall = () => {
- return new Promise ( (resolve, reject) => {
-  // async call with callback. seudo code
-  // http.request('http://google.com')
-  // http.on('error', err => reject(err))
-  // http.on('end', data => resolve(data))
- })
-}
-
-main()
+main().then(successOutput).catch(failureOutput)
